eb065ff11858c416cb65c41356cdb3c087894a11
Kelso <graemebrooks@gmail.com>
2021-02-16 23:15:05 -0600

    streamlines GoogleSheet and GoogleCredentialProvider classes

--- src/main/java/com/wafflarchiveapi/API/GoogleCredentialProvider.java
+++ src/main/java/com/wafflarchiveapi/API/GoogleCredentialProvider.java
@@ -1,13 +1,8 @@
 package com.wafflarchiveapi.API;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.*;
 import java.security.GeneralSecurityException;
@@ -16,52 +11,13 @@ import java.util.List;
 
 public class GoogleCredentialProvider {
 
-    private static Sheets sheetsService;
-    private static final String APPLICATION_NAME = "waffl-archive";
-
-    private static Credential authorize () throws IOException, GeneralSecurityException {
-        InputStream in = GoogleCredentialProvider.class.getResourceAsStream("/credentials.json");
-
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                JacksonFactory.getDefaultInstance(), new InputStreamReader(in)
-        );
-
-        List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
-
-        Credential credential = GoogleCredential
+    public static Credential authorize () throws IOException, GeneralSecurityException {
+        return GoogleCredential
                 .fromStream(new FileInputStream("src/main/resources/credentials.json"))
-                .createScoped(SCOPES);
-
-        return credential;
+                .createScoped(getScopes(SheetsScopes.SPREADSHEETS_READONLY));
     }
 
-    public static Sheets getSheetsService() throws IOException,
-            GeneralSecurityException {
-        Credential credential = authorize();
-        return new Sheets.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(), credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+    private static List<String> getScopes(String sheetScopeOption) {
+        return Collections.singletonList(sheetScopeOption);
     }
-
-    public void getSheetValues(String spreadsheetId, String range) throws IOException,
-            GeneralSecurityException {
-        sheetsService = getSheetsService();
-
-
-        ValueRange response =
-                sheetsService.spreadsheets().values().get(spreadsheetId, range).execute();
-
-        List<List<Object>> values = response.getValues();
-
-        if (values == null || values.isEmpty()) {
-            System.out.println("No data found.");
-        } else {
-            for (List row : values) {
-                System.out.println(row.get(0));
-            }
-        }
-    }
-
 }
--- src/main/java/com/wafflarchiveapi/API/GoogleSheet.java
+++ src/main/java/com/wafflarchiveapi/API/GoogleSheet.java
@@ -1,32 +1,37 @@
 package com.wafflarchiveapi.API;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.List;
 
 
 public class GoogleSheet {
 
-    public GoogleSheet(String spreadsheetId, String range) throws IOException, GeneralSecurityException {
+    private static final String APPLICATION_NAME = "waffl-archive";
+    public Sheets sheetsService;
 
+    public GoogleSheet() throws IOException,
+            GeneralSecurityException {
+        this.sheetsService = getSheetsService();
+    }
+
+    public List<List<Object>> getSheetValuesFromRange(String spreadsheetId, String range)
+            throws IOException {
+        ValueRange response = sheetsService.spreadsheets().values().get(spreadsheetId, range).execute();
+        return response.getValues();
+    }
+
+    private Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        return new Sheets.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(), GoogleCredentialProvider.authorize())
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 
 }
